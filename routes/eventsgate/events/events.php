15f28714e854fb27fc48602111be0d15770201e7
B-asmala <basmalabayoumi@gmail.com>
2026-02-24 21:44:56 +0200

    refactor: separate event participants controller and router

--- routes/eventsgate/events/events.php
+++ routes/eventsgate/events/events.php
@@ -8,7 +8,4 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::apiResource('events', EventController::class);
 
-    Route::post('events/{event}/register', [EventController::class, 'registerUser']);
-    Route::delete('events/{event}/unregister', [EventController::class, 'unregisterUser']);
-
 });
